fabd4ea81b4224383fed5d1ddc9b59807bd42abb
Blue Eye <satyabrata02@gmail.com>
2026-06-11 23:53:19 +0530

    Updated: Customer Rewards Service

--- customer-rewards-service/src/main/java/com/cap/rewards/model/Transaction.java
+++ customer-rewards-service/src/main/java/com/cap/rewards/model/Transaction.java
@@ -5,16 +5,66 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-public record Transaction(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        Long id,
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        String customerName,
-        LocalDate date,
-        double amount,
+    private String customerName;
+    private LocalDate date;
+    private double amount;
 
-        @ManyToOne
-        @JoinColumn(name = "customer_id")
-        Customer customer
-) {}
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    public Transaction() {}
+
+    public Transaction(Long id, String customerName, LocalDate date, double amount, Customer customer) {
+        this.id = id;
+        this.customerName = customerName;
+        this.date = date;
+        this.amount = amount;
+        this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
